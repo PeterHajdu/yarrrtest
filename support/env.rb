7b8b9d0d4f7b43d3f7fcea35b21ab131243afac6
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-04-09 14:58:59 +0200

    sync support code with server tests

--- support/env.rb
+++ support/env.rb
@@ -1,2 +1,10 @@
 require 'rspec/expectations'
+require_relative './process_runner'
+
+redis_process = ProcessRunner.new( {}, 'redis-server' )
+redis_process.start
+
+at_exit do
+  redis_process.kill
+end
 
--- support/hooks.rb
+++ support/hooks.rb
@@ -1,5 +1,23 @@
+require 'redis'
+
+Before do
+  @notification_file_path = "./notifications"
+  @notification_file = File.open( @notification_file_path, "w+" )
+  @db = Redis.new
+  @db.flushdb
+end
 
 After do
-  @yarrr_client.kill
+  @yarrr_server.kill
+  @yarrr_client.kill if not @yarrr_client.nil?
+
+  if not @yarrr_clients.nil? then
+
+    @yarrr_clients.each do | client |
+      client.kill
+    end
+
+  end
+
 end
 
